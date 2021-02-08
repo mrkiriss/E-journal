5c5bd0dd4c34c4847e21962c4282faa01392fa3d
kirill <kvoskrebentsev@mail.ru>
2021-02-08 22:43:03 +0300

    Git...

--- app/src/main/java/com/example/e_journal/interfaces/JournalPostman.java
+++ app/src/main/java/com/example/e_journal/interfaces/JournalPostman.java
@@ -1,13 +1,11 @@
 package com.example.e_journal.interfaces;
 
-<<<<<<< HEAD
+
 import com.example.e_journal.school.Class;
 import com.example.e_journal.school.School;
 
 public interface JournalPostman {
     public School getSchool();
+
     public void updateClasses(Class c);
-=======
-public interface JournalPostman {
->>>>>>> main
 }
